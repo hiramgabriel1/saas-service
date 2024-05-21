058e0aed38bb4b03501a6af14598e5aca1562c53
itsrusty <rusty3031@gmail.com>
2024-05-21 13:37:38 -0700

    feat: add config security to main.ts and change app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,19 @@ import { ProductsClientModule } from './products-client/products-client.module';
 import { ProductsModule } from './products/products.module';
 import { AssistantsModule } from './assistants/assistants.module';
 import config from './configuration/config';
+import { AssistantsController } from './assistants/assistants.controller';
+import { EmployeesController } from './employees/employees.controller';
+import { ManagmentController } from './managment/managment.controller';
+import { ProductsClientController } from './products-client/products-client.controller';
+import { ProductsController } from './products/products.controller';
+import { TablesController } from './tables/tables.controller';
+import { AdminService } from './admin/admin.service';
+import { AssistantsService } from './assistants/assistants.service';
+import { EmployeesService } from './employees/employees.service';
+import { ProductsClientService } from './products-client/products-client.service';
+import { ProductsService } from './products/products.service';
+import { TablesService } from './tables/tables.service';
+import { ManagmentService } from './managment/managment.service';
 
 @Module({
   imports: [
@@ -31,8 +44,23 @@ import config from './configuration/config';
     ProductsModule,
     AssistantsModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [
+    AssistantsController,
+    EmployeesController,
+    ManagmentController,
+    ProductsClientController,
+    ProductsController,
+    TablesController,
+  ],
+  providers: [
+    AdminService,
+    AssistantsService,
+    EmployeesService,
+    ProductsClientService,
+    ProductsService,
+    ManagmentService,
+    TablesService,
+  ],
 })
 
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
-  const PORT = configService.get('PORT')
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
 
-  app.setGlobalPrefix('api/v1')
-  app.enableCors()
-  app.useGlobalPipes(new ValidationPipe())
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:4000', 'https://nuvix.vercel.app'],
+  });
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        },
+      },
+    }),
+  );
 
   await app.listen(PORT || 5000);
   console.log(`http://localhost:${PORT}`);
